ead9bbb4fc02269bd43203285a0c28054038b87a
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2024-11-04 21:58:10 -0800

    created an async function for sign in

--- farm_records_management/lib/screens/authenticate/sign_in.dart
+++ farm_records_management/lib/screens/authenticate/sign_in.dart
@@ -4,3 +4,44 @@ class SignInPage extends StatefulWidget {
   @override
   State<SignInPage> createState() => _SignInPageState();
 }
+
+Future<void> _signUpWithEmail() async {
+  if (_formKey.currentState?.validate() ?? false) {
+    String email = _emailController.text.trim();
+    String password = _passwordController.text.trim();
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      User? user = await _auth.signUpWithEmailAndPassword(email, password);
+      if (user != null) {
+        if (mounted) {
+          logger.i('Sign up successful');
+          Navigator.pushReplacementNamed(context, '/crops');
+        }
+      } else {
+        if (mounted) {
+          logger.w('Error during sign-up');
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Sign-up failed')),
+          );
+        }
+      }
+    } catch (e) {
+      if (mounted) {
+        logger.e('Error during sign-up: $e');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error during sign-up: $e')),
+        );
+      }
+    } finally {
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
+}
